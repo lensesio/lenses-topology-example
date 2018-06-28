648b269b2dad40e3ec61b2ade88cb1f2208a522b
sksamuel <sam@sksamuel.com>
2018-06-28 19:20:01 +0100

    Updated examples for fixes

--- lenses-topology-example-sparkstreaming/src/main/java/com/landoop/lenses/topology/example/spark/kafka/App.java
+++ lenses-topology-example-sparkstreaming/src/main/java/com/landoop/lenses/topology/example/spark/kafka/App.java
@@ -16,6 +16,7 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.OutputMode;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 
@@ -78,6 +79,7 @@ public class App {
 
         StreamingQuery query = wordCounts.writeStream()
                 .format("kafka")
+                .outputMode(OutputMode.Update())
                 .option("checkpointLocation", "/tmp/checkpoint")
                 .option("kafka.bootstrap.servers", "localhost:9092")
                 .option("topic", outputTopic)
